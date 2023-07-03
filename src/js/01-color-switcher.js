08415e14a3c07179bd2e477bc83f08e630f8cbf8
charivna <124678194+charivna@users.noreply.github.com>
2023-07-03 14:31:30 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+const refs = {
+    start: document.querySelector('button[data-start]'),
+    stop: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body')
+}
+
+let intervalId = null;
+const INTERVAL = 1000;
+
+refs.start.addEventListener('click', onStartClick);
+refs.stop.addEventListener('click', onStopClick);
+
+
+function onStartClick() {
+    intervalId = setInterval(() => {
+        const color = getRandomHexColor();
+        refs.body.style.backgroundColor = color;
+    }, INTERVAL)
+    refs.start.setAttribute('disabled', '');
+    refs.stop.disabled = false
+}
+
+function onStopClick() {
+    clearInterval(intervalId);
+    refs.stop.setAttribute('disabled', '')
+    refs.start.disabled = false
+}
 
